1a5efaad9c314f4d38f44b20149f745e009144d4
Evan Slaton <evanslaton@gmail.com>
2019-02-04 12:28:28 -0800

    Can signup/login, and new users are adde are added to the user collection in Firebase

--- app/src/main/java/com/hwang/xsighting/MainActivity.java
+++ app/src/main/java/com/hwang/xsighting/MainActivity.java
@@ -1,13 +1,125 @@
 package com.hwang.xsighting;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.IdpResponse;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.hwang.xsighting.models.User;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    private static FirebaseUser user;
+    private static final int RC_SIGN_IN = 482;
+    private static final String TAG = "MainActivity";
 
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
-  }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        user = FirebaseAuth.getInstance().getCurrentUser();
+            if (user != null) {
+                // User is signed in
+                setContentView(R.layout.activity_main);
+
+                TextView showName = findViewById(R.id.greeting);
+                showName.setText("Hello " + user.getDisplayName());
+
+                Log.i(TAG, user.toString());
+
+            } else {
+                // No user is signed in
+                // Direct them to Login
+                Log.i(TAG, "About to launch sign in");
+                // Choose authentication providers
+                List<AuthUI.IdpConfig> providers = Arrays.asList(
+                      new AuthUI.IdpConfig.EmailBuilder().build());
+
+                // Create and launch sign-in intent
+                startActivityForResult(
+                      AuthUI.getInstance()
+                              .createSignInIntentBuilder()
+                              .setAvailableProviders(providers)
+                              .build(),
+                      RC_SIGN_IN);
+
+                Log.i(TAG, "Intent Sent");
+            }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RC_SIGN_IN) {
+            IdpResponse response = IdpResponse.fromResultIntent(data);
+
+            if (resultCode == RESULT_OK) {
+                // Successfully signed in
+                // Render the page with projects
+                user = FirebaseAuth.getInstance().getCurrentUser();
+
+                // Checks to see if the current user has a document in the "users" collection and creates one if not
+                final FirebaseFirestore db = FirebaseFirestore.getInstance();
+                DocumentReference docRef = db.collection("users").document(FirebaseAuth.getInstance().getUid());
+                docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                        if (task.isSuccessful()) {
+                            DocumentSnapshot document = task.getResult();
+                            if (document.exists()) {
+                                Log.d(TAG, "DocumentSnapshot data: " + document.getData());
+                            } else {
+                                Log.d(TAG, "No such document, creating user");
+
+                                // Saves user to Firebase
+                                db.collection("users").document(FirebaseAuth.getInstance().getUid())
+                                        .set(new User())
+                                        .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                            @Override
+                                            public void onSuccess(Void aVoid) {
+                                                Log.d(TAG, "DocumentSnapshot successfully written!");
+                                            }
+                                        })
+                                        .addOnFailureListener(new OnFailureListener() {
+                                            @Override
+                                            public void onFailure(@NonNull Exception e) {
+                                                Log.w(TAG, "Error writing document", e);
+                                            }
+                                        });
+                            }
+                        } else {
+                            Log.d(TAG, "get failed with ", task.getException());
+                        }
+                    }
+                });
+
+                setContentView(R.layout.activity_main);
+                TextView showName = findViewById(R.id.greeting);
+                showName.setText("Hello " + user.getDisplayName());
+
+            } else {
+                // Sign in failed. If response is null the user canceled the
+                // sign-in flow using the back button. Otherwise check
+                // response.getError().getErrorCode() and handle the error.
+                // ...
+            }
+        }
+    }
 }
