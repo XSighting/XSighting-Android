500f8988d91f3985bf3ca3042879430d1b5217fe
Jeff Borda <jeffborda@outlook.com>
2019-02-04 12:05:00 -0800

    created new activity and button

---
+++ app/src/main/java/com/hwang/xsighting/CreateSighting.java
@@ -0,0 +1,20 @@
+package com.hwang.xsighting;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class CreateSighting extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_sighting);
+    }
+
+
+
+
+}
--- app/src/main/java/com/hwang/xsighting/MainActivity.java
+++ app/src/main/java/com/hwang/xsighting/MainActivity.java
@@ -1,7 +1,10 @@
 package com.hwang.xsighting;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,10 @@ public class MainActivity extends AppCompatActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+  }
+
+  public void goToCreateSighting(View v) {
+    Intent createSightingIntent = new Intent(this, CreateSighting.class);
+    startActivity(createSightingIntent);
   }
 }
