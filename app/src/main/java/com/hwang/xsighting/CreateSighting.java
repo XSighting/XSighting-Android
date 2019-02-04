4aea13224e61ab7dbfad6491d210711ee905f6b7
Jeff Borda <jeffborda@outlook.com>
2019-02-04 13:40:35 -0800

    developing sighting report activity

--- app/src/main/java/com/hwang/xsighting/CreateSighting.java
+++ app/src/main/java/com/hwang/xsighting/CreateSighting.java
@@ -6,12 +6,28 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.firebase.firestore.FirebaseFirestore;
+
 public class CreateSighting extends AppCompatActivity {
+
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private FusedLocationProviderClient fusedLocationClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_sighting);
+
+        // Get Location
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+        getLocation();
+    }
+
+
+    public void getLocation() {
+        
     }
 
 
