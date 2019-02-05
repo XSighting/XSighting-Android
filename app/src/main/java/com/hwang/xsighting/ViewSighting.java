6fa00b25188960525e86f039f6d547058e4f8318
Jessica Lovell <jessicaamberlovell@gmail.com>
2019-02-05 13:39:43 -0800

    create view

--- app/src/main/java/com/hwang/xsighting/ViewSighting.java
+++ app/src/main/java/com/hwang/xsighting/ViewSighting.java
@@ -3,8 +3,11 @@ package com.hwang.xsighting;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -39,17 +42,45 @@ public class ViewSighting extends AppCompatActivity {
                         Log.d(TAG, "DocumentSnapshot data: " + document.getData());
                         sightingToDisplay = document.toObject(Sighting.class);
 
-                        // Set Fields and other things
+                        // Set Text fields with the sighting
+                        TextView date = findViewById(R.id.postDate);
+                        TextView location = findViewById(R.id.postLocation);
+                        TextView user = findViewById(R.id.postUser);
+                        TextView description = findViewById(R.id.postDescription);
+
+                        date.setText(sightingToDisplay.getTimestamp().toString());
+                        location.setText(sightingToDisplay.getLocation());
+                        user.setText(sightingToDisplay.getAuthorUsername());
+                        description.setText(sightingToDisplay.getDescription());
+
+
                     } else {
                         Log.d(TAG, "No such document");
 
                         //TODO: Redirect to the main page with a failure toast
+
+                        Context context = getApplicationContext();
+                        CharSequence text = "Something went wrong! Try Again.";
+                        int duration = Toast.LENGTH_SHORT;
+
+                        Toast toast = Toast.makeText(context, text, duration);
+                        toast.show();
+                        finish();
                     }
                 } else {
                     Log.d(TAG, "get failed with ", task.getException());
                     //TODO: Redirect to the main page with a failure toast
+
+                    Context context = getApplicationContext();
+                    CharSequence text = "Something went wrong! Try Again";
+                    int duration = Toast.LENGTH_SHORT;
+
+                    Toast toast = Toast.makeText(context, text, duration);
+                    toast.show();
+                    finish();
                 }
             }
         });
     }
+
 }
