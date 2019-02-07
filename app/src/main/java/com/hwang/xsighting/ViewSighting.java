982acdf97576db656cac0769fe6882c7f325f0ca
Jessica Lovell <jessicaamberlovell@gmail.com>
2019-02-06 17:24:05 -0800

    update sview detail

--- app/src/main/java/com/hwang/xsighting/ViewSighting.java
+++ app/src/main/java/com/hwang/xsighting/ViewSighting.java
@@ -78,20 +78,23 @@ public class ViewSighting extends AppCompatActivity {
                         sightingToDisplay = document.toObject(Sighting.class);
 
                         // Set Text fields with the sighting
-                        TextView date = findViewById(R.id.postDate);
-                        TextView location = findViewById(R.id.postLocation);
+//                        TextView date = findViewById(R.id.postDate);
+//                        TextView location = findViewById(R.id.postLocation);
                         TextView user = findViewById(R.id.postUser);
                         TextView description = findViewById(R.id.postDescription);
+                        TextView title = findViewById(R.id.postTitle);
 
                         // Make the Date String Pretty
                         Date toDate = sightingToDisplay.getCreatedTime().toDate();
                         SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yy HH:mm");
                         String stringOfTime = dateFormat.format(toDate);
 
-                        date.setText(stringOfTime);
-                        location.setText(sightingToDisplay.getLocationName());
-                        user.setText(sightingToDisplay.getAuthorUsername());
+                        //TODO: fix this date format?
+//                        date.setText(stringOfTime);
+//                        location.setText(sightingToDisplay.getLocationName());
+                        user.setText(sightingToDisplay.getAuthorUsername() );
                         description.setText(sightingToDisplay.getDescription());
+                        title.setText("Sighted in " +sightingToDisplay.getLocationName());
                     } else {
                         Log.d(TAG, "No such document");
 
