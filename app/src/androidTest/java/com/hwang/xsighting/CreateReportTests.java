1d5ca3c9d8b77ce0070442d30168f111fa6361ef
Jeff Borda <jeffborda@outlook.com>
2019-02-08 08:55:54 -0800

    commit for PR

--- app/src/androidTest/java/com/hwang/xsighting/CreateReportTests.java
+++ app/src/androidTest/java/com/hwang/xsighting/CreateReportTests.java
@@ -5,8 +5,8 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import androidx.test.filters.LargeTest;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.filters.LargeTest;
 import androidx.test.rule.ActivityTestRule;
 
 import static androidx.test.espresso.Espresso.onView;
