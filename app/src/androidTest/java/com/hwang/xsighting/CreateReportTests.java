e7273611f462cf0c2aba2b791b5588850f6b042c
Jeff Borda <jeffborda@outlook.com>
2019-02-07 20:07:21 -0800

    developing tests for create sighting activity

--- app/src/androidTest/java/com/hwang/xsighting/CreateReportTests.java
+++ app/src/androidTest/java/com/hwang/xsighting/CreateReportTests.java
@@ -30,7 +30,7 @@ public class CreateReportTests {
     @Rule
     public ActivityTestRule<CreateSighting> createSightingActivity = new ActivityTestRule<>(CreateSighting.class);
 
-    // UI Tests
+    // Submit Button
     @Test
     public void submitReportButtonIsEnabled() {
         onView(withId(R.id.button_submit_report)).check(matches(isEnabled()));
@@ -61,21 +61,119 @@ public class CreateReportTests {
         onView(withId(R.id.button_submit_report)).check(matches(withText(R.string.report_submit)));
     }
 
+    // Take Picture Button
+    @Test
+    public void takePictureButtonIsEnabled() {
+        onView(withId(R.id.takePictureButton)).check(matches(isEnabled()));
+    }
+
+    @Test
+    public void takePictureButtonIsDisplayed() {
+        onView(withId(R.id.takePictureButton)).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void takePictureButtonIsCompletelyDisplayed() {
+        onView(withId(R.id.takePictureButton)).check(matches(isCompletelyDisplayed()));
+    }
+
+    @Test
+    public void takePictureButtonIsNotSelectable() {
+        onView(withId(R.id.takePictureButton)).check(matches(not(isSelected())));
+    }
+
+    @Test
+    public void takePictureButtonWithText() {
+        onView(withId(R.id.takePictureButton)).check(matches(withText(R.string.report_take_picture)));
+    }
+
+    @Test
+    public void takePictureButtonIsClickable() {
+        onView(withId(R.id.takePictureButton)).check(matches(isClickable()));
+    }
+
+    // Choose Picture Button
+    @Test
+    public void choosePictureButtonIsEnabled() {
+        onView(withId(R.id.choosePictureButton)).check(matches(isEnabled()));
+    }
+
+    @Test
+    public void choosePictureButtonIsDisplayed() {
+        onView(withId(R.id.choosePictureButton)).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void choosePictureButtonIsCompletelyDisplayed() {
+        onView(withId(R.id.choosePictureButton)).check(matches(isCompletelyDisplayed()));
+    }
+
+    @Test
+    public void choosePictureButtonIsNotSelectable() {
+        onView(withId(R.id.choosePictureButton)).check(matches(not(isSelected())));
+    }
+
+    @Test
+    public void choosePictureButtonWithText() {
+        onView(withId(R.id.choosePictureButton)).check(matches(withText(R.string.report_choose_picture)));
+    }
+
+    @Test
+    public void choosePictureButtonIsClickable() {
+        onView(withId(R.id.choosePictureButton)).check(matches(isClickable()));
+    }
+
+    // Header TextView
     @Test
     public void headerReportSightingWithText() {
         onView(withId(R.id.header_report_sighting)).check(matches(withText(R.string.header_report_sighting)));
     }
 
     @Test
-    public void reportDescriptionLabelWithText() {
-        onView(withId(R.id.report_description_label)).check(matches(withText(R.string.report_description_long)));
+    public void headerReportSightingIsDisplayed() {
+        onView(withId(R.id.header_report_sighting)).check(matches(isDisplayed()));
     }
 
     @Test
-    public void editTextHint(){
-        onView(withId(R.id.report_description)).check(matches(withHint("@string/report_description")));
+    public void headerReportSightingIsCompletelyDisplayed() {
+        onView(withId(R.id.header_report_sighting)).check(matches(isCompletelyDisplayed()));
+    }
 
+    @Test
+    public void headerReportSightingIsNotSelectable() {
+        onView(withId(R.id.header_report_sighting)).check(matches(not(isSelected())));
+    }
 
+    // Description Label TextView
+    @Test
+    public void reportDescriptionLabelWithText() {
+        onView(withId(R.id.report_description_label)).check(matches(withText(R.string.description_tag)));
+    }
+
+    @Test
+    public void reportDescriptionLabelIsDisplayed() {
+        onView(withId(R.id.report_description_label)).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void reportDescriptionLabelIsCompletelyDisplayed() {
+        onView(withId(R.id.report_description_label)).check(matches(isCompletelyDisplayed()));
+    }
+
+    @Test
+    public void reportDescriptionLabelIsNotSelectable() {
+        onView(withId(R.id.report_description_label)).check(matches(not(isSelected())));
+    }
+
+    // Description EditText Field
+    @Test
+    public void reportDescriptionWithText() {
+        onView(withId(R.id.report_description_label)).check(matches(withText(R.string.description_tag)));
+    }
+
+    @Test
+    public void reportDescriptionLabelWithHint(){
+        onView(withId(R.id.report_description)).check(matches(withHint(R.string.include_color_shape_of_object_sounds_etc)));
     }
 
 
