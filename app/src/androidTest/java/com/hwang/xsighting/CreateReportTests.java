36b4bfcf1c6440e983c295a05d8201be0be33c07
Jeff Borda <jeffborda@outlook.com>
2019-02-07 20:44:11 -0800

    create sighting tests complete

--- app/src/androidTest/java/com/hwang/xsighting/CreateReportTests.java
+++ app/src/androidTest/java/com/hwang/xsighting/CreateReportTests.java
@@ -1,17 +1,17 @@
 package com.hwang.xsighting;
 
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-
-import java.io.IOException;
-import java.io.OutputStream;
 
 import androidx.test.filters.LargeTest;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.rule.ActivityTestRule;
 
 import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.isClickable;
 import static androidx.test.espresso.matcher.ViewMatchers.isCompletelyDisplayed;
@@ -29,36 +29,121 @@ public class CreateReportTests {
 
     @Rule
     public ActivityTestRule<CreateSighting> createSightingActivity = new ActivityTestRule<>(CreateSighting.class);
+    private String stringToBeTyped;
 
-    // Submit Button
+    @Before
+    public void initValidString() {
+        // Specify a valid string.
+        stringToBeTyped = "Espresso testing 123";
+    }
+
+
+    /*
+     * UI elements tested in the order they appear on the CreateSighting activity from top to bottom.
+     */
+
+    // Header TextView
     @Test
-    public void submitReportButtonIsEnabled() {
-        onView(withId(R.id.button_submit_report)).check(matches(isEnabled()));
+    public void headerReportSightingWithText() {
+        onView(withId(R.id.header_report_sighting)).check(matches(withText(R.string.header_report_sighting)));
     }
 
     @Test
-    public void submitReportButtonIsDisplayed() {
-        onView(withId(R.id.button_submit_report)).check(matches(isDisplayed()));
+    public void headerReportSightingIsDisplayed() {
+        onView(withId(R.id.header_report_sighting)).check(matches(isDisplayed()));
     }
 
     @Test
-    public void submitReportButtonIsCompletelyDisplayed() {
-        onView(withId(R.id.button_submit_report)).check(matches(isCompletelyDisplayed()));
+    public void headerReportSightingIsCompletelyDisplayed() {
+        onView(withId(R.id.header_report_sighting)).check(matches(isCompletelyDisplayed()));
     }
 
     @Test
-    public void submitReportButtonIsNotSelectable() {
-        onView(withId(R.id.button_submit_report)).check(matches(not(isSelected())));
+    public void headerReportSightingIsNotSelectable() {
+        onView(withId(R.id.header_report_sighting)).check(matches(not(isSelected())));
+    }
+
+    // Description Label TextView
+    @Test
+    public void reportDescriptionLabelWithText() {
+        onView(withId(R.id.report_description_label)).check(matches(withText(R.string.description_tag)));
     }
 
     @Test
-    public void submitReportButtonIsClickable() {
-        onView(withId(R.id.button_submit_report)).check(matches(isClickable()));
+    public void reportDescriptionLabelIsDisplayed() {
+        onView(withId(R.id.report_description_label)).check(matches(isDisplayed()));
     }
 
     @Test
-    public void submitReportButtonWithText() {
-        onView(withId(R.id.button_submit_report)).check(matches(withText(R.string.report_submit)));
+    public void reportDescriptionLabelIsCompletelyDisplayed() {
+        onView(withId(R.id.report_description_label)).check(matches(isCompletelyDisplayed()));
+    }
+
+    @Test
+    public void reportDescriptionLabelIsNotSelectable() {
+        onView(withId(R.id.report_description_label)).check(matches(not(isSelected())));
+    }
+
+    // Description EditText Field
+    @Test
+    public void reportDescriptionWithHint(){
+        onView(withId(R.id.report_description)).check(matches(withHint(R.string.include_color_shape_of_object_sounds_etc)));
+    }
+
+    @Test
+    public void reportDescriptionIsEnabled() {
+        onView(withId(R.id.report_description)).check(matches(isEnabled()));
+    }
+
+    @Test
+    public void reportDescriptionIsDisplayed() {
+        onView(withId(R.id.report_description)).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void reportDescriptionIsCompletelyDisplayed() {
+        onView(withId(R.id.report_description)).check(matches(isCompletelyDisplayed()));
+    }
+
+    @Test
+    public void reportDescriptionIsNotSelectable() {
+        onView(withId(R.id.report_description)).check(matches(not(isSelected())));
+    }
+
+    @Test
+    public void reportDescriptionIsClickable() {
+        onView(withId(R.id.report_description)).check(matches(isClickable()));
+    }
+
+    @Test
+    public void reportDescriptionCanTypeText() {
+        // Type text
+        onView(withId(R.id.report_description))
+                .perform(typeText(stringToBeTyped), closeSoftKeyboard());
+        // Check that the text was changed
+        onView(withId(R.id.report_description))
+                .check(matches(withText(stringToBeTyped)));
+    }
+
+    // Add Photo Label TextView
+    @Test
+    public void addPhotoLabelWithText() {
+        onView(withId(R.id.addPhoto)).check(matches(withText(R.string.add_photo_tag)));
+    }
+
+    @Test
+    public void addPhotoLabelIsDisplayed() {
+        onView(withId(R.id.addPhoto)).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void addPhotoLabelIsCompletelyDisplayed() {
+        onView(withId(R.id.addPhoto)).check(matches(isCompletelyDisplayed()));
+    }
+
+    @Test
+    public void addPhotoLabelIsNotSelectable() {
+        onView(withId(R.id.addPhoto)).check(matches(not(isSelected())));
     }
 
     // Take Picture Button
@@ -123,60 +208,50 @@ public class CreateReportTests {
         onView(withId(R.id.choosePictureButton)).check(matches(isClickable()));
     }
 
-    // Header TextView
+    // Report Photo - Before Adding Photo
     @Test
-    public void headerReportSightingWithText() {
-        onView(withId(R.id.header_report_sighting)).check(matches(withText(R.string.header_report_sighting)));
+    public void reportPhotoIsNotDisplayed() {
+        onView(withId(R.id.sightingImage)).check(matches(not(isDisplayed())));
     }
 
     @Test
-    public void headerReportSightingIsDisplayed() {
-        onView(withId(R.id.header_report_sighting)).check(matches(isDisplayed()));
+    public void reportPhotoIsCompletelyNotDisplayed() {
+        onView(withId(R.id.sightingImage)).check(matches(not(isCompletelyDisplayed())));
     }
 
     @Test
-    public void headerReportSightingIsCompletelyDisplayed() {
-        onView(withId(R.id.header_report_sighting)).check(matches(isCompletelyDisplayed()));
+    public void reportPhotoIsNotSelectable() {
+        onView(withId(R.id.sightingImage)).check(matches(not(isSelected())));
+    }
+
+    // Submit Button
+    @Test
+    public void submitReportButtonIsEnabled() {
+        onView(withId(R.id.button_submit_report)).check(matches(isEnabled()));
     }
 
     @Test
-    public void headerReportSightingIsNotSelectable() {
-        onView(withId(R.id.header_report_sighting)).check(matches(not(isSelected())));
-    }
-
-    // Description Label TextView
-    @Test
-    public void reportDescriptionLabelWithText() {
-        onView(withId(R.id.report_description_label)).check(matches(withText(R.string.description_tag)));
+    public void submitReportButtonIsDisplayed() {
+        onView(withId(R.id.button_submit_report)).check(matches(isDisplayed()));
     }
 
     @Test
-    public void reportDescriptionLabelIsDisplayed() {
-        onView(withId(R.id.report_description_label)).check(matches(isDisplayed()));
+    public void submitReportButtonIsCompletelyDisplayed() {
+        onView(withId(R.id.button_submit_report)).check(matches(isCompletelyDisplayed()));
     }
 
     @Test
-    public void reportDescriptionLabelIsCompletelyDisplayed() {
-        onView(withId(R.id.report_description_label)).check(matches(isCompletelyDisplayed()));
+    public void submitReportButtonIsNotSelectable() {
+        onView(withId(R.id.button_submit_report)).check(matches(not(isSelected())));
     }
 
     @Test
-    public void reportDescriptionLabelIsNotSelectable() {
-        onView(withId(R.id.report_description_label)).check(matches(not(isSelected())));
-    }
-
-    // Description EditText Field
-    @Test
-    public void reportDescriptionWithText() {
-        onView(withId(R.id.report_description_label)).check(matches(withText(R.string.description_tag)));
+    public void submitReportButtonIsClickable() {
+        onView(withId(R.id.button_submit_report)).check(matches(isClickable()));
     }
 
     @Test
-    public void reportDescriptionLabelWithHint(){
-        onView(withId(R.id.report_description)).check(matches(withHint(R.string.include_color_shape_of_object_sounds_etc)));
+    public void submitReportButtonWithText() {
+        onView(withId(R.id.button_submit_report)).check(matches(withText(R.string.report_submit)));
     }
-
-
-
-
 }
